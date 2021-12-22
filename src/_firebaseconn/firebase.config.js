6709f563530bda9046c22a4ed6a2aa2a95b28dd2
haerulmutakin <haerulm.takin@gmail.com>
2021-12-22 09:13:36 +0700

    configure firebase connection

---
+++ src/_firebaseconn/firebase.config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_F_API_KEY,
+  authDomain: process.env.REACT_APP_F_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_F_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_F_STORAGE_BUCKER,
+  messagingSenderId: process.env.REACT_APP_F_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_F_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
