6b532e3765084cf4e54c4a138c28adda75ef8d17
haerulmutakin <haerulm.takin@gmail.com>
2021-12-22 15:03:23 +0700

    add menu summary

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faCalendar, faChartLine, faListAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return ( 
@@ -6,9 +8,18 @@ const Navbar = () => {
             <div className="nav">
                 <h1>Zaida Finance</h1>
                 <div className="menu">
-                    <NavLink to="/" exact activeClassName="active">Home</NavLink>
-                    <NavLink to="/monthly" activeClassName="active">Bulanan</NavLink>
-                    <NavLink to="/survey" activeClassName="active">Survey</NavLink>
+                    <NavLink to="/" exact activeClassName="active">
+                        <FontAwesomeIcon icon={faClock} />
+                    </NavLink>
+                    <NavLink to="/monthly" activeClassName="active">
+                        <FontAwesomeIcon icon={faCalendar} />
+                    </NavLink>
+                    <NavLink to="/summary" activeClassName="active">
+                        <FontAwesomeIcon icon={faChartLine} />
+                    </NavLink>
+                    <NavLink to="/survey" activeClassName="active">
+                        <FontAwesomeIcon icon={faListAlt} />
+                    </NavLink>
                 </div>
             </div>
         </nav>
