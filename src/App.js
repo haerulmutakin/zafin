3e79d2cc5c170d2a0268cb3d48ff4c968f676850
haerulmutakin <haerulm.takin@gmail.com>
2021-12-22 09:13:10 +0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Survey from 'pages/Survey';
+import Graph from 'pages/Graph';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/monthly" component={Graph} />
+            <Route path="/survey" component={Survey} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
