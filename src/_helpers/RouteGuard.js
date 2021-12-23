9d4d6387cf75334eeb8246246fed980e9d828507
haerulmutakin <haerulm.takin@gmail.com>
2021-12-23 18:49:50 +0700

    add routeguard

---
+++ src/_helpers/RouteGuard.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../_provider/AuthProvider';
+
+const RouteGuard = ({component: RouteComponent, ...rest}) => {
+    const user = useContext(AuthContext);
+    return (
+        <Route 
+            {...rest} 
+            render = {props => {
+                console.log(user);
+                if (user) {
+                    return <RouteComponent {...props} />
+                } else {
+                    return <Redirect to={'/login'} />
+                }
+            }}
+        />
+    )
+}
+
+export default RouteGuard;
