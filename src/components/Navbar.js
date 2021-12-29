3cc474ff70f877726cee95961055a7db5fbd601f
haerulmutakin <haerulm.takin@gmail.com>
2021-12-29 16:44:31 +0700

    add menu label

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,15 +10,19 @@ const Navbar = () => {
                 <div className="menu">
                     <NavLink to="/" exact activeClassName="active">
                         <FontAwesomeIcon icon={faClock} />
+                        <label>Harian</label>
                     </NavLink>
                     <NavLink to="/monthly" activeClassName="active">
                         <FontAwesomeIcon icon={faCalendar} />
+                        <label>Bulanan</label>
                     </NavLink>
                     <NavLink to="/survey" activeClassName="active">
                         <FontAwesomeIcon icon={faListAlt} />
+                        <label>Survey</label>
                     </NavLink>
                     <NavLink to="/summary" activeClassName="active">
                         <FontAwesomeIcon icon={faChartLine} />
+                        <label>Ikhtisar</label>
                     </NavLink>
                 </div>
             </div>
