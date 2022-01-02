6b0951dbda1ca3ff2f82bd09dc065a628e6e2de9
haerulmutakin <haerulm.takin@gmail.com>
2022-01-02 19:22:28 +0700

    show year name in summary page

--- src/pages/summary.js
+++ src/pages/summary.js
@@ -3,7 +3,7 @@ import YearSummary from "components/YearSummary";
 const Summary = () => {
     return ( 
         <div className="fitwidth">
-            <h4>Pengeluaran tiap bulan</h4>
+            <h4>Pengeluaran tiap bulan di tahun {new Date().getFullYear()}</h4>
             <YearSummary />
         </div>
      );
