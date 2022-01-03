cc630ca6a2b3954c83c486deda5828de37f7bdcd
haerulmutakin <haerulm.takin@gmail.com>
2022-01-03 09:13:08 +0700

    remove firebase messaging

--- src/_firebaseconn/firebase.config.js
+++ src/_firebaseconn/firebase.config.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 import { getFirestore } from 'firebase/firestore';
 
 var firebaseConfig = {
@@ -13,21 +12,5 @@ var firebaseConfig = {
 
 const firebase = initializeApp(firebaseConfig);
 export const firebaseDB = getFirestore();
-
-const messaging = getMessaging(firebase);
-// onMessage(())
-getToken(messaging, { vapidKey: 'BJS5I4C8Fr2cCtu10mYpC02oxhHeeCnGnLRUbJrExW5XAADSuBZLXqed6GiKGBxO61Z4LQ8CVszuzg6a11mDtqQ' }).then((currentToken) => {
-  if (currentToken) {
-    // Send the token to your server and update the UI if necessary
-    // ...
-  } else {
-    // Show permission request UI
-    console.log('No registration token available. Request permission to generate one.');
-    // ...
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-  // ...
-});
 
 export default firebase;
