addc4416ac6f0393ef1b465e092b24928fcc719b
haerulmutakin <haerulm.takin@gmail.com>
2022-08-04 10:57:01 +0700

    refactoring env variable name

--- src/_firebaseconn/firebase.config.js
+++ src/_firebaseconn/firebase.config.js
@@ -5,7 +5,7 @@ var firebaseConfig = {
   apiKey: process.env.REACT_APP_F_API_KEY,
   authDomain: process.env.REACT_APP_F_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_F_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_F_STORAGE_BUCKER,
+  storageBucket: process.env.REACT_APP_F_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_F_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_F_APP_ID,
 };
