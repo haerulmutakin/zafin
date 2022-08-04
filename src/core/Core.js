64056a01e1715d70e99d6d4a3831e78555a4a22a
haerulmutakin <haerulm.takin@gmail.com>
2022-08-04 11:00:26 +0700

    remove survey page and component

--- src/core/Core.js
+++ src/core/Core.js
@@ -2,7 +2,6 @@ import { Route, Switch } from 'react-router-dom';
 import RecapProvider from '_provider/RecapProvider';
 import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
-import Survey from '../pages/Survey';
 import Monthly from '../pages/Monthly';
 import Summary from 'pages/summary';
 import Profile from 'pages/Profile';
@@ -15,7 +14,6 @@ const Core = () => {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/monthly" component={Monthly} />
-                <Route path="/survey" component={Survey} />
                 <Route path="/summary" component={Summary} />
                 <Route path="/profile" component={Profile} />
             </Switch>
